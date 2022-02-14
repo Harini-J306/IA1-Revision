977603152ef660b140c25ca3809a6ad7fe51323b
Harini Janardhan <94678338+Harini-J306@users.noreply.github.com>
2022-02-14 14:46:51 +0000

    done

---
+++ p2final.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void input(int *a,int *b,int *c)
+{
+    printf("enter three numbers\n");
+    scanf("%d%d%d",a,b,c);
+}
+void cmp(int a,int b,int c,int *large)
+{
+    *large=0;
+  if(a>b && a>c)
+  *large=a;
+  else if(b>a && b>c)
+  *large=b;
+  else
+  *large=c;
+}
+void output(int large)
+{
+    printf("%d is greatest\n",large);
+}
+int main()
+{
+    int a,b,c,large;
+    input(&a,&b,&c);
+    cmp(a,b,c,&large);
+    output(large);
+    return 0;
+}
